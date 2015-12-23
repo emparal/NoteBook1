a32495887729f91bec7a6ee0a38cac4b07074afd
emanuelhailu <emparalhailu@yahoo.com>
2015-12-23 11:17:33 +0300

    Added a note controller and resources

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,2 +1,32 @@
 class NotesController < ApplicationController
+
+	def index
+	end
+
+	def new
+	end
+
+	def create
+	end
+
+     def update
+     end
+
+	def show
+	end
+
+	def destroy
+	end
+
+   private
+
+   def find_note
+   end
+
+   def note_params
+   end
+   
+
+end
+
 end
